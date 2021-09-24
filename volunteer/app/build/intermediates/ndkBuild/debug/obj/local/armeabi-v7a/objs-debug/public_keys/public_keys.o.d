9df202522146989561c4ff86a0575e30054d982e
jjbae <jejun1983@gmail.com>
2021-09-24 14:30:49 +0900

     Apply .gitignore

--- volunteer/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/public_keys/public_keys.o.d
+++
@@ -1,2 +0,0 @@
-/Users/jjbae/Desktop/backup/volunteer/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/public_keys/public_keys.o: \
-  /Users/jjbae/Desktop/backup/volunteer/app/src/main/jni/public_keys.c
--- volunteer/app/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/public_keys/public_keys.o.d
+++
@@ -1,2 +0,0 @@
-/Users/jjbae/Desktop/backup/volunteer/app/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/public_keys/public_keys.o: \
-  /Users/jjbae/Desktop/backup/volunteer/app/src/main/jni/public_keys.c
